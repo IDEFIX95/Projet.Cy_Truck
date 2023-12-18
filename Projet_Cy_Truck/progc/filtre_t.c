476b240ef384d382aabf0dfb21b76b27772ec732
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-12-18 16:18:55 +0100

    Add files via upload

---
+++ Projet_Cy_Truck/progc/filtre_t.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 256
+#define MAX_VILLE_LENGTH 50
+
+
+
+int main() {
+printf("heloo world");
+
+    return 0;
+}
---
+++ Projet_Cy_Truck/progc/tris_fonctions.h
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <sys/stat.h>  // Pour les fichiers en arguments
+#include <string.h>     // Pour strcmp (--abr et tout en argument)
+#include <unistd.h>     // Pour acces (verifie si on a le droit de lecture sur un fichier en argument)
+#include <math.h>
+#define BUFFER_SIZE 512
