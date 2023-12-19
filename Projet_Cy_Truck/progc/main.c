8249cc4efefdc9988ab69e6cdf56fcf570177e1b
NaTwiX27 <133356569+NaTwiX27@users.noreply.github.com>
2023-12-19 16:16:32 +0100

    Update main.c

--- Projet_Cy_Truck/progc/main.c
+++ Projet_Cy_Truck/progc/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>  // Pour les fichiers en arguments
 #include <string.h>     // Pour strcmp (--abr et tout en argument)
-#include <unistd.h>     // Pour acces (verifie si on a le droit de lecture sur un fichier en argument)
+#include <unistd.h>     // Pour accès (verifie si on a le droit de lecture sur un fichier en argument)
 #include <math.h>
 #define BUFFER_SIZE 512
 
@@ -13,10 +13,10 @@ int main(int argc, char * argv[]){
 
     for (int i = 1; i < argc; i++) {
         if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-s") == 0){
-            printf("Ces options font bien partir des options disponible\n");
+            printf("Ces options font bien partie des options disponible\n");
         }
         else{
-            printf("Cette options ne fait pas partis des options disponible pour le tri en C\n");
+            printf("Cette option ne fait pas partie des options disponibles pour le tri en C\n");
         }
     }
     return 0;
