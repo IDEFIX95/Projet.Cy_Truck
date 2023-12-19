698e319836b5707f66a9b096aaadde8419e0f0ec
NaTwiX27 <133356569+NaTwiX27@users.noreply.github.com>
2023-12-19 16:01:01 +0100

    Update tris_fonctions.h
    Mise à jour : 
    
    - Structures Arbres et AVL

--- Projet_Cy_Truck/progc/tris_fonctions.h
+++ Projet_Cy_Truck/progc/tris_fonctions.h
@@ -5,3 +5,17 @@
 #include <unistd.h>     // Pour acces (verifie si on a le droit de lecture sur un fichier en argument)
 #include <math.h>
 #define BUFFER_SIZE 512
+
+
+typedef struct arbre{
+	int valeur;
+	struct arbre *fg;
+	struct arbre *fd;
+}Arbre;
+
+typedef struct avl{
+	int valeur;
+	struct AVL *fg;
+	struct AVL *fd;
+	int equilibre;
+}AVL;
