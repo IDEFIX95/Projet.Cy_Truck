372eacb02da17c379d49bd9568e5bd91fbc4a14d
MonsieurNikko <151777338+MonsieurNikko@users.noreply.github.com>
2023-12-19 15:59:07 +0100

    Update donnee_filtree.bash
    Mise à jour : 
    - Condition de vérification du logiciel ImageMaker
    - L'ajout de Gnuplot

--- Projet_Cy_Truck/donnee_filtree.bash
+++ Projet_Cy_Truck/donnee_filtree.bash
@@ -4,7 +4,7 @@ make all > /dev/null 2> $
 cd ..
 
 
-# Variables des Options
+# Variables des Options.
 
 
 
@@ -18,7 +18,27 @@ option_t="-t"
 
 option_oblig=0
 
+#--------------------------------------------------------------------------------------------------------------#
 
+# Installation du logiciel ImageMagic afin de manipuler les graphiques.
+if command -v convert > /dev/null 2>&1; then
+  echo "ImageMagick est installé."
+else
+  echo "ImageMagick n'est pas installé. Installation en cours..."
+  
+  # Installation ImageMagick.
+  sudo apt-get update
+  sudo apt-get install imagemagick
+  
+  # Verification de l'installation.
+  if [ $? -eq 0 ]; then
+    echo "ImageMagick a été installé avec succès."
+  else
+    echo "Erreur lors de l'installation de ImageMagick."
+  fi
+fi
+
+#--------------------------------------------------------------------------------------------------------------#
 
 if [ "$1" == $fichier_d_entrer ]; then     # Regarde si le 1er argument de la ligne de commande est bien le chemin vers le fichier de donnée d'entrer.
     echo "Le chemin vers le fichier $fichier_d_entrer est bien le premier argument de la ligne de commande."
@@ -27,25 +47,26 @@ else
     exit 1
 fi
 
+#--------------------------------------------------------------------------------------------------------------#
 
-# Vérifier si le fichier source C existe
+# Vérifier si le fichier source C existe.
 if [ -e "progc/main.c" ]; then
     echo "Le fichier source C main.c est présent."
     if [ ! -e "progc/main" ]; then
-    # Se déplacer dans le répertoire progc
+    # Se déplacer dans le répertoire progc.
     cd progc
 
-    # Compilation du fichier C
+    # Compilation du fichier C.
     make all
 
-    # Vérification du statut de la compilation
+    # Vérification du statut de la compilation.
     if [ $? -eq 0 ]; then
         echo "La compilation s'est bien déroulée. L'exécutable main a été créé."
     else
         echo "Erreur lors de la compilation."
     fi
 
-    # Revenir au répertoire initial
+    # Revenir au répertoire initial.
    cd ..
     else 
         echo "L'executable C est present sur le disque dur"
@@ -54,6 +75,8 @@ else
     echo "Le fichier source C main.c n'est pas trouvé. Lancement de la compilation."
 fi
 
+#--------------------------------------------------------------------------------------------------------------#
+
 if [[ ! -d "$dossier_images" && ! -d "$dossier_temp" ]];then
     mkdir images
     mkdir temp
@@ -61,9 +84,9 @@ else
     rm -f temp/*
 fi
 
+#--------------------------------------------------------------------------------------------------------------#
 
-
-##Gestion des Options (Parcour de la la ligne de Commande pour reperer les Options)
+##Gestion des Options (Parcour de la la ligne de Commande pour reperer les Options).
 
 for i in "$@" ;do                             # La condition qui me permet de lancer l'option aide et donc de faire apparaitre le fichier help.
     if [ "$i" == $fichier_d_entrer ] ;then
@@ -75,55 +98,138 @@ for i in "$@" ;do                             # La condition qui me permet de la
         exit 1  
     fi
     if [ "$i" == "-d1" ];then
-        # Enregistrez le temps de début
+        # Enregistrez le temps de début.
         debut_timer_d1=$(date +%s)
         
-        awk -F";" '/;1;/ {compteur[$6] += 1} END {for (nom in compteur) print nom ";" compteur[nom]}' data/data.csv |sort -t";" -k2nr > demo/d1_final.csv
+        awk -F";" '/;1;/ {compteur[$6] += 1} END {for (nom in compteur) print nom ";" compteur[nom]}' data/data.csv |sort -t";" -k2nr | head -10 > demo/d1_final.csv
         
         fin_timer_d1=$(date +%s)
 
-        # Calculez la durée totale en secondes
+        # Calculez la durée totale en secondes.
         duree_option_d1=$((fin_timer_d1 - debut_timer_d1))
 
         option_oblig=$(("$option_oblig"+1))
 
-        # Affichez la durée
+        # Affichez la durée.
         echo -e "\nLe traitement de l'option -d1 a pris $duree_option_d1 secondes.\n"
+
+        #Partie graphique (gnuplot).
+        gnuplot << EOF
+
+        # Paramètres de sortie
+        set terminal pngcairo enhanced font 'Arial,10'
+        set output 'images/histogramme_d1.png'
+
+        # Paramètres du graphique
+        set bmargin 13    # Ajuster la marge inférieure (en unités par défaut)
+        set rmargin 10
+        set lmargin 2
+        set title 'Histogramme de traitement d1'
+        set xlabel 'Conducteurs'
+        set ylabel 'Nombre de trajets'
+        set xtic rotate by 90 offset 0,-9
+        set xlabel rotate by 180 offset 0,-9
+        set ylabel offset 92,0
+        set ytic offset 83,0
+        set yrange [0:250]
+
+        set ytic rotate by 90
+
+        set style histogram rowstacked
+        set style fill solid border -1
+        set boxwidth 0.5
+
+        # Tracé du graphique
+        set datafile separator ';'
+        plot 'demo/d1_final.csv' using 2:xtic(1) with boxes lc rgb 'blue'
+
+EOF
+    convert -rotate 90 images/histogramme_d1.png images/histogramme_d1.png  # Commande pour pivoter l'image afin de transformer en un histogramme horizontal.
+    display images/histogramme_d1.png
     fi
 
     if [ "$i" == "-d2" ];then
-        # Enregistrez le temps de début
+        # Enregistrez le temps de début.
         debut_timer_d2=$(date +%s)
-    
-        #awk -F";" ' NR > 1 {print $5 ";" $6}' data/data.csv > progc/d2_final1.csv
-        #touch progc/d2_vraifinal.csv
-        #./progc/tri_d2
-        #awk -F";" '/;1;/ {compteur[$6] += $5} END {for (nom in compteur) print nom ";" compteur[nom]}' data/data.csv |sort -t";" -k2nr | head -10 > demo/d2_final.csv
         awk -F";" '{compteur[$6] += $5} END {for (nom in compteur) print nom ";" compteur[nom]}' data/data.csv |sort -t";" -k2nr | head -10 > demo/d2_final.csv
         fin_timer_d2=$(date +%s)
 
-        # Calculez la durée totale en secondes
+        # Calculez la durée totale en secondes.
         duree_option_d2=$((fin_timer_d2 - debut_timer_d2))
 
         option_oblig=$(("$option_oblig"+1))
 
-        # Affichez la durée
+        # Affichez la durée.
         echo -e "\nLe traitement de l'option -d2 a pris $duree_option_d2 secondes.\n"
+
+        # Partie graphique (gnuplot).
+        gnuplot << EOF
+
+        # Paramètres de sortie
+        set terminal pngcairo enhanced font 'Arial,10'
+        set output 'images/histogramme_d2.png'
+
+        # Paramètres du graphique
+        set bmargin 13
+        set rmargin 10
+        set lmargin 2
+        set title 'Histogramme de traitement d2'
+        set xlabel 'Conducteurs'
+        set ylabel 'Distance totale (en kilomètres)'
+        set xtic rotate by 90 offset 0,-9
+        set xlabel rotate by 180 offset 0,-9
+        set ylabel offset 92,0
+        set ytic offset 83,0
+        set yrange [0:200000]
+
+        set ytic rotate by 90
+
+        set style histogram rowstacked
+        set style fill solid border -1
+        set boxwidth 0.5
+
+        # Tracé du graphique
+        set datafile separator ';'
+        plot 'demo/d2_final.csv' using 2:xtic(1) with boxes lc rgb 'blue'
+
+EOF
+    convert -rotate 90 images/histogramme_d2.png images/histogramme_d2.png  # Commande pour pivoter l'image afin de transformer en un histogramme horizontal.
+    display images/histogramme_d2.png
     fi
 
     if [ "$i" == "-l" ];then
-        # Enregistrez le temps de début
+        # Enregistrez le temps de début.
         debut_timer_l=$(date +%s)
         awk -F";"  '{compteur[$1] += $5} END {for (id_trajet in compteur) print id_trajet ";" compteur[id_trajet]}' data/data.csv |sort -t";" -k2nr | head -10 | sort -t";" -k1n > demo/l_final.csv
         fin_timer_l=$(date +%s)
 
-        # Calculez la durée totale en secondes
+        # Calculez la durée totale en secondes.
         duree_option_l=$((fin_timer_l - debut_timer_l))
 
         option_oblig=$(("$option_oblig"+1))
 
-        # Affichez la durée
+        # Affichez la durée.
         echo -e "\nLe traitement de l'option -l a pris $duree_option_l secondes.\n"
+
+        # Partie graphique (gnuplot).
+        gnuplot << EOF
+        # Paramètres de sortie
+        set terminal pngcairo enhanced font 'Arial,10'
+        set output 'images/histogramme_l.png'
+
+        # Paramètres du graphique
+        set title 'Histogramme de traitement l'
+        set xlabel 'Identifiant du trajet (route ID)'
+        set ylabel 'Distance (en kilomètres)'
+        set style histogram rowstacked
+        set style fill solid border -1
+        set boxwidth 0.5
+
+        # Tracé du graphique
+        set datafile separator ';'
+        plot 'demo/l_final.csv' using 2:xtic(1) with boxes lc rgb 'blue'
+EOF
+        display images/histogramme_l.png
     fi
 
     if [ "$i" == $option_t ];then
@@ -144,6 +250,8 @@ for i in "$@" ;do                             # La condition qui me permet de la
     fi
 
 done
+
+#--------------------------------------------------------------------------------------------------------------#
 
 if (( "$option_oblig" < 2 )); then
    echo "pas assez d'arguments dans la ligne de commande"
