2f99db58848c06576849f493b3de77162fcb6d9e
MonsieurNikko <151777338+MonsieurNikko@users.noreply.github.com>
2024-02-01 18:21:30 +0100

    Update etape1_filtre_t.c

--- Projet_Cy_Truck/progc/etape1_filtre_t.c
+++ Projet_Cy_Truck/progc/etape1_filtre_t.c
@@ -1,8 +1,6 @@
 // CE PROGRAMME PERMET D'EXTRAIRE LA COLONNE 4 DU FICHIER "DATA.CSV" VERS UN AUTRE FICHIER AFIN DE TRAVAILLER SUR LE NOMBRE DE TRAJETS DES VILLES.
 // ÉTAPE 1
 
-// Modifier cette partie pour pouvoir supprimer les doublons de ville A; ville B tels que Marseille;Marseille si c'est le cas ignorer cette ligne sinon 
-// Ajouter la ville d'arrivée au fichier de sortie
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,8 +9,83 @@
 #define TAILLE_BUFFER 1024
 #define FICHIER_ENTREE "data/data1.csv"
 #define FICHIER_SORTIE "temp/fichier_col4.csv"
+// Utilisation d'un ABR pour filtrer car il y a des doublons dans le fichier data.csv (comme MARSEILLE;MARSEILLE ou MARSEILLE peut apparaître plusieurs fois dans un même ID)
+typedef struct ABR{
+    char *ville;
+    char *ID;
+    struct ABR* fg;
+    struct ABR* fd;
+}ABR,*pABR;
 
-void copierdonnee(FILE *fichierEntree, FILE *fichierSortie) {
+
+pABR creerarbre(char *ID, char *ville) {
+    ABR *new = malloc(sizeof(*new));
+    if (new == NULL) {
+        printf("Erreur d'allocation dans creerarbre pour ID: %s, ville: %s\n", ID, ville);
+        exit(EXIT_FAILURE);
+    }
+
+    // Utiliser strdup pour allouer et copier la chaîne
+    new->ID = strdup(ID);
+    if (new->ID == NULL) {
+        printf("Erreur d'allocation pour ID: %s\n", ID);
+        free(new);
+        exit(EXIT_FAILURE);
+    }
+    new->ville = strdup(ville);
+    if (new->ville == NULL) {
+        printf("Erreur d'allocation pour ville: %s\n", ville);
+        free(new->ID);
+        free(new);
+        exit(EXIT_FAILURE);
+    }
+
+    new->fd = new->fg = NULL;
+    return new;
+}
+
+
+
+pABR insertABR(pABR a, char *ID, char* ville){
+    if (a == NULL){
+        a = creerarbre(ID,ville);
+    } 
+    else { // Condition d'ajoutement en fonction d'ID et de la ville pour enlever les villes qui apparaissent deux fois dans un même trajet
+        int compareID = strcmp(ID, a->ID);
+        if (compareID < 0){
+            a->fg = insertABR(a->fg,ID,ville);
+        } 
+        else if (compareID > 0){
+            a->fd = insertABR(a->fd,ID,ville);
+        } 
+        else {
+            int compareville = strcmp(ville, a->ville);
+            if (compareville < 0){
+                a->fg = insertABR(a->fg, ID, ville);
+            } 
+            else if (compareville > 0){
+                a->fd = insertABR(a->fd, ID, ville);
+            } 
+            else {
+                // Même ID et même ville, on l'ignore
+                return a;
+            }
+        }
+    }
+    return a;
+}
+
+// Fonction pour écrire la colonne 4 filtrée dans le fichier ficher_col4.csv
+void parcours(pABR a, FILE *fichier) {
+    if (a != NULL) {
+        parcours(a->fd, fichier);
+        fprintf(fichier, "%s\n", a->ville);
+        parcours(a->fg, fichier);
+    } 
+}
+
+// Fonction pour filtrer les doublons comme MARSEILLE;MARSEILLE et inserer la colonne 4 dans l'ABR.
+void copierdonnee(pABR *a, FILE *fichierEntree) {
     char buffer[TAILLE_BUFFER];
     int isFirstLine = 1; 
     while (fgets(buffer, sizeof(buffer), fichierEntree) != NULL) {
@@ -27,32 +100,42 @@ void copierdonnee(FILE *fichierEntree, FILE *fichierSortie) {
         while (token != NULL){
             char *token2 = strtok(token,";");
             int colonne = 1;
+            char tempcol1[TAILLE_BUFFER];
             char tempcol3[TAILLE_BUFFER];
             char tempcol4[TAILLE_BUFFER];
             while (token2 != NULL){
-                if (colonne == 3){
+                if (colonne == 1){
+                    strcpy(tempcol1,token2);
+                } else if (colonne == 3){
                     strcpy(tempcol3,token2);
                 } else if (colonne == 4){
                     strcpy(tempcol4,token2);
                     if (strcmp(tempcol4,tempcol3) != 0){
-                        fprintf(fichierSortie, "%s", token2);
+                    *a = insertABR(*a,tempcol1,tempcol4);
                     }
-                }
+                } 
                 token2 = strtok(NULL, ";");
                 colonne++;
             }
-            fprintf(fichierSortie, "\n");
             token = strtok(NULL, "\n");
         }
 
+    }
+}
 
+// Fonction pour libérer l'arbre
+void libererABR(pABR a) {
+    if (a != NULL) {
+        libererABR(a->fg);
+        libererABR(a->fd);
+        free(a);
     }
 }
 
 
 int main(int argc, char* argv[]) {
     FILE *fichier1, *fichier2;
-
+    pABR a = NULL;
     if(argc==1){
         printf("Il y a le bon nombre d'argument de la ligne de commande est correct donc le chemin vers le fichier data '%s' est present.", argv[1]);
     }
@@ -70,9 +153,11 @@ int main(int argc, char* argv[]) {
         fclose(fichier1);
         return 1;
     }
-    printf("abc.\n");
 
-    copierdonnee(fichier1, fichier2);
+
+    copierdonnee(&a, fichier1); // Extraire et filtrer la colonne 4
+    parcours(a,fichier2); // Importer la colonne filtrée dans le fichier_col4.csv
+    libererABR(a); // Liberer l'arbre
     
 
     // Fermer les fichiers
@@ -84,7 +169,7 @@ int main(int argc, char* argv[]) {
     }
 
 
-    printf("Traitement terminé avec succès.\n");
+    printf("Traitement de l'étape 1 terminé avec succès.\n");
 
     return 0;
 }
