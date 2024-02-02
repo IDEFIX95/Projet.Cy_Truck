ae2db6e6918e66841d2437e0e1b5453fdc7127cc
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2024-02-02 17:41:29 +0100

    Update etape1_filtre_t.c

--- Projet_Cy_Truck/progc/etape1_filtre_t.c
+++ Projet_Cy_Truck/progc/etape1_filtre_t.c
@@ -9,14 +9,7 @@
 #define TAILLE_BUFFER 1024
 #define FICHIER_ENTREE "data/data1.csv"
 #define FICHIER_SORTIE "temp/fichier_col4.csv"
-// Utilisation d'un ABR pour filtrer car il y a des doublons dans le fichier data.csv (comme MARSEILLE;MARSEILLE ou MARSEILLE peut apparaître plusieurs fois dans un même ID)
-typedef struct ABR{
-    char *ville;
-    char *ID;
-    struct ABR* fg;
-    struct ABR* fd;
-}ABR,*pABR;
-
+#include "etape1_filtre_t.h"
 
 pABR creerarbre(char *ID, char *ville) {
     ABR *new = malloc(sizeof(*new));
